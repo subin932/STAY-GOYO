f89cc88d0d4b06145d91bc0cc7db2dcb2ab38836
subin932 <asubin1345@naver.com>
2025-04-07 09:39:36 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+  $(".sub_wrap").hide();
+  $(".gnb>li").mouseenter(function(){
+    $(this).children(".sub_wrap").stop().slideDown();
+   });
+   $(".gnb>li").mouseleave(function(){
+     $(this).children(".sub_wrap").stop().slideUp();
+   });
+});
